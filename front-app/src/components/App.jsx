67ac0867c62417be27c58d6f9fc79f51d3d17ee9
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-07-18 19:35:26 +0200

    chore(front-app): add front app initial scaffolding

---
+++ front-app/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  state = {
+  };
+
+render() {
+    return (
+        <p> Hello Crypto booking app </p>
+          );
+  }
+}
+
+module.exports = App;
---
+++ front-app/webpack/vendor.js
@@ -0,0 +1,13 @@
+const vendor = [
+  'babel-polyfill',
+  'react',
+  'react-dom',
+  'react-router',
+  'react-virtualized',
+  'react-hot-loader',
+  'lodash',
+  'moment',
+  'react-loadable',
+];
+
+module.exports = vendor;
