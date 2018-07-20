701689d513f78322eab0708c90eb98d8fce5f417
Nico Calabrese <nico.sikker@gmail.com>
2018-07-20 13:33:17 -0300

    link to css framework

--- front-app/src/index.jsx
+++ front-app/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
+import 'windingtree-ui/dist/styles.css';
 
 const render = (Component) => {
   ReactDOM.render(
