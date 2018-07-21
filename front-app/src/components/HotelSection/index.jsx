a42c41c2599f0427eb18a90a568ef530950fd9a1
Pablo Alice <pabloalice95@gmail.com>
2018-07-21 18:37:39 -0300

    define Hotel Container structure

--- front-app/src/components/HotelSection/index.jsx
+++ front-app/src/components/HotelSection/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Description from './Description';
 
-const HotelSection = () => (
-  <div>
-      Hotel
-  </div>
-);
+class HotelContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
 
-export default HotelSection;
+  async componentDidMount() {
+    // TODO here get data form server :(hotel description, rooms(description, price, isAvailable))
+    const response = await (new Promise());
+    this.setState({ isLoading: false, hotelData: response });
+  }
+
+  render() {
+    const { hotelData, isLoading } = this.state;
+    if (isLoading) return null; // TODO here should be some Loading component
+    return (
+      <div>
+        <Description description={hotelData.description} />
+        {/* TODO rooms should be rendered here */}
+      </div>
+    );
+  }
+}
+
+export default HotelContainer;
