25a53b61eeae4037580bbd64befe2052c6a87eb0
Nico Calabrese <nico.sikker@gmail.com>
2018-07-24 06:39:24 -0300

    Added css classes to improve the layout

--- front-app/src/components/BookingSection/RoomBooking.jsx
+++ front-app/src/components/BookingSection/RoomBooking.jsx
@@ -17,65 +17,73 @@ export default class RoomBooking extends React.Component {
       onSubmit,
     } = this.props;
     return (
-      <div id='BookARoom' style={{ margin: 'auto', width: '500px' }}>
-        <h2>
-            Book a Room
-        </h2>
-        <p>
-            All rooms offered to book only within ETH Berlin Dates: 6.09 - 10.09
-        </p>
-        <form onSubmit={onSubmit}>
-          <Section>
-            <label htmlFor="twin" style={{ marginRight: 8 }}>
-                Twin Room
-            </label>
-            <input className="form-control form-control-lg" id="twin" style={{ display: 'inline-block', marginBottom: '5px' }} name="type" type="radio" value="twin" onChange={onRoomTypeChange} required/>
-          </Section>
-          <Section>
-            <label htmlFor="double" style={{ marginRight: 8 }}>
-                Double Room
-            </label>
-            <input className="form-control form-control-lg" id="double" style={{ display: 'inline-block', marginBottom: '5px' }} name="type" type="radio" value="double" onChange={onRoomTypeChange} />
-          </Section>
-          <Section>
-            <input className="form-control form-control-lg" style={{ marginRight: '5px' }} type="date" min="2018-09-06" max="2018-09-09" onChange={onFromDateChange} value={from} required/>
-            <input className="form-control form-control-lg" type="date" name="to" min={toDateMin} max="2018-09-10" onChange={onToDateChange} required/>
-          </Section>
-          <Section>
-            <label htmlFor="guestAddress" style={labelBlockStyle}>
-                Guest Eth Address
-            </label>
-            <input className="form-control form-control-lg" id="guestAddress" style={{ marginRight: '5px' }} type="text" onChange={onAddressChange} placeholder='0xe99356bde974bbe08721d77712168fa070aa8da2' required/>
-          </Section>
-          <Section>
-            <div style={{ display: 'inline-block', marginRight: 32 }}>
-              <label htmlFor="fullName" style={labelBlockStyle}>
-                  Full Name
-              </label>
-              <input className="form-control form-control-lg" id="fullName" style={{ marginRight: '5px' }} type="text" onChange={onFullNameChange} required placeholder='Pedrotti Capone'/>
-              <label htmlFor="birthDate" style={labelBlockStyle}>
-                  birth Date
-              </label>
-              <input className="form-control form-control-lg" id="birthDate" style={{ marginRight: '5px' }} type="date" onChange={onBirthDateChange} />
+      <article id='BookARoom' className="section-wrapper bg-light py-3 py-md-5">
+        <div className="container">
+          <div classnName="row justify-content-center">
+            <div classnName="col-md-8">
+
+              <h2>
+                  Book a Room
+              </h2>
+              <p>
+                  All rooms offered to book only within ETH Berlin Dates: 6.09 - 10.09
+              </p>
+              <form onSubmit={onSubmit}>
+                <Section>
+                  <label htmlFor="twin" style={{ marginRight: 8 }}>
+                      Twin Room
+                  </label>
+                  <input className="form-control form-control-lg" id="twin" style={{ display: 'inline-block', marginBottom: '5px' }} name="type" type="radio" value="twin" onChange={onRoomTypeChange} required/>
+                </Section>
+                <Section>
+                  <label htmlFor="double" style={{ marginRight: 8 }}>
+                      Double Room
+                  </label>
+                  <input className="form-control form-control-lg" id="double" style={{ display: 'inline-block', marginBottom: '5px' }} name="type" type="radio" value="double" onChange={onRoomTypeChange} />
+                </Section>
+                <Section>
+                  <input className="form-control form-control-lg" style={{ marginRight: '5px' }} type="date" min="2018-09-06" max="2018-09-09" onChange={onFromDateChange} value={from} required/>
+                  <input className="form-control form-control-lg" type="date" name="to" min={toDateMin} max="2018-09-10" onChange={onToDateChange} required/>
+                </Section>
+                <Section>
+                  <label htmlFor="guestAddress" style={labelBlockStyle}>
+                      Guest Eth Address
+                  </label>
+                  <input className="form-control form-control-lg" id="guestAddress" style={{ marginRight: '5px' }} type="text" onChange={onAddressChange} placeholder='0xe99356bde974bbe08721d77712168fa070aa8da2' required/>
+                </Section>
+                <Section>
+                  <div style={{ display: 'inline-block', marginRight: 32 }}>
+                    <label htmlFor="fullName" style={labelBlockStyle}>
+                        Full Name
+                    </label>
+                    <input className="form-control form-control-lg" id="fullName" style={{ marginRight: '5px' }} type="text" onChange={onFullNameChange} required placeholder='Pedrotti Capone'/>
+                    <label htmlFor="birthDate" style={labelBlockStyle}>
+                        birth Date
+                    </label>
+                    <input className="form-control form-control-lg" id="birthDate" style={{ marginRight: '5px' }} type="date" onChange={onBirthDateChange} />
+                  </div>
+                  <div style={{ display: 'inline-block' }}>
+                    <label htmlFor="email" style={labelBlockStyle}>
+                        Email
+                    </label>
+                    <input className="form-control form-control-lg" id="email" style={{ marginRight: '5px' }} type="email" onChange={onEmailChange} required placeholder='someGuy@windingtree.com'/>
+                    <label htmlFor="phone" style={labelBlockStyle}>
+                        Phone Number
+                    </label>
+                    <input className="form-control form-control-lg" id="phone" style={{ marginRight: '5px' }} type="tel" onChange={onPhoneChange} placeholder='+54 011 1135989272'/>
+                  </div>
+                </Section>
+                <Section>
+                  <button className="form-control form-control-lg" type="submit">
+                    BOOK
+                  </button>
+                </Section>
+              </form>
+
             </div>
-            <div style={{ display: 'inline-block' }}>
-              <label htmlFor="email" style={labelBlockStyle}>
-                  Email
-              </label>
-              <input className="form-control form-control-lg" id="email" style={{ marginRight: '5px' }} type="email" onChange={onEmailChange} required placeholder='someGuy@windingtree.com'/>
-              <label htmlFor="phone" style={labelBlockStyle}>
-                  Phone Number
-              </label>
-              <input className="form-control form-control-lg" id="phone" style={{ marginRight: '5px' }} type="tel" onChange={onPhoneChange} placeholder='+54 011 1135989272'/>
-            </div>
-          </Section>
-          <Section>
-            <button className="form-control form-control-lg" type="submit">
-              BOOK
-            </button>
-          </Section>
-        </form>
-      </div>
+          </div>
+        </div>
+      </article>
     );
   }
 }
