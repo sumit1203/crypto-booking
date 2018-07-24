52ca34fafe758c881d5dc90c48390af8d6a6ae26
skkr <nico.sikker@gmail.com>
2018-07-24 06:36:51 -0300

    Markup for Paying by Lif section. Added logo images.

--- front-app/src/components/LifSection/index.jsx
+++ front-app/src/components/LifSection/index.jsx
@@ -1,9 +1,87 @@
 import React from 'react';
 
+import logoIdex from "../../img/content/cards/idex.svg";
+import logoYobit from "../../img/content/cards/yobit.png";
+import logoOpenledger from "../../img/content/cards/openledger.png";
+import logoForkdelta from "../../img/content/cards/forkdelta.png";
+
 const LifSection = () => (
-  <div>
-      LifSection
-  </div>
+  <article className="py-3 py-md-5">
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+
+          <h2 className="mb-2 mb-md-4 text-center">
+            Paying by Lif
+          </h2>
+          <p className="lead mb-2 mb-md-3 text-center text-md-left">
+              There were 24,976,541.45 Lífs generated and distributed between team and backers. No&nbsp;new token generation expected, so&nbsp;the only way to&nbsp;get Lífs is&nbsp;to&nbsp;buy from existing holders on&nbsp;cryptocurrency exchanges.
+          </p>
+
+          <div className="card-deck">
+
+            <a href="https://idex.market/" className="card card-sm">
+              <div className="card-header d-none d-md-block text-center">
+                  <img src={logoIdex} alt="IDEX" className="img-fluid"/>
+              </div>
+
+              <div className="card-body d-flex flex-column small">
+                <h6 className="card-title text-muted">
+                  <img src={logoIdex} alt="IDEX" className="d-md-none"/>
+                  IDEX
+                  <i className="mdi mdi-24px mdi-chevron-right d-md-none"></i>
+                </h6>
+              </div>
+            </a>
+
+            <a href="http://yobit.io/" className="card card-sm">
+              <div className="card-header d-none d-md-block text-center">
+                <img src={logoYobit} alt="YObit" className="img-fluid"/>
+              </div>
+
+              <div className="card-body d-flex flex-column small">
+                <h6 className="card-title text-muted">
+                  <img src={logoYobit} alt="YObit" className="d-md-none"/>
+                  YObit
+                  <i className="mdi mdi-24px mdi-chevron-right d-md-none"></i>
+                </h6>
+              </div>
+            </a>
+
+            <a href="https://openledger.info/" className="card card-sm">
+              <div className="card-header d-none d-md-block text-center">
+                <img src={logoOpenledger} alt="Open Ledger" className="img-fluid"/>
+              </div>
+
+              <div className="card-body d-flex flex-column small">
+                <h6 className="card-title text-muted">
+                  <img src={logoOpenledger} alt="Open Ledger" className="d-md-none"/>
+                  Open Ledger
+                  <i className="mdi mdi-24px mdi-chevron-right d-md-none"></i>
+                </h6>
+              </div>
+            </a>
+
+            <a href="https://forkdelta.github.io/" className="card card-sm">
+              <div className="card-header d-none d-md-block text-center">
+                <img src={logoForkdelta} alt="ForkDelta" className="img-fluid"/>
+              </div>
+
+              <div className="card-body d-flex flex-column small">
+                <h6 className="card-title text-muted">
+                  <img src={logoForkdelta} alt="ForkDelta" className="d-md-none"/>
+                  ForkDelta
+                  <i className="mdi mdi-24px mdi-chevron-right d-md-none"></i>
+                </h6>
+              </div>
+            </a>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </article>
 );
 
 export default LifSection;
