eb6796f0824de5b817ff12502c395f17adef43a6
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-07-28 21:11:08 +0300

    chore(config): add mailgun config

--- server/src/config/develop.js
+++ server/src/config/develop.js
@@ -4,4 +4,8 @@ module.exports = {
   SERVER_PORT: 3001,
   STARTING_BLOCK: 3668521,
   MONGODB_URI: process.env.MONGODB_URI || 'localhost:27017/crypto-booking-test',
+  MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
+  MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN,
+  MAILGUN_FROM_EMAIL: process.env.MAILGUN_FROM_EMAIL,
+  MAILGUN_TO_EMAIL: process.env.MAILGUN_TO_EMAIL,
 };
--- server/src/config/test.js
+++ server/src/config/test.js
@@ -4,4 +4,8 @@ module.exports = {
   SERVER_PORT: 3001,
   STARTING_BLOCK: 3668521,
   MONGODB_URI: process.env.MONGODB_URI || 'localhost:27017/crypto-booking-test',
+  MAILGUN_API_KEY: 'API-KEY',
+  MAILGUN_DOMAIN: 'sandboxexample.mailgun.org',
+  MAILGUN_FROM_EMAIL: 'example@windingtree.com',
+  MAILGUN_TO_EMAIL: 'example@windingtree.com',
 };
