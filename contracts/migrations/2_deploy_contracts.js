00c102b2486adbbdc96a2ce91cd9e5b0ce6a5edf
AugustoL <augusto.lemble@gmail.com>
2018-08-03 22:02:35 +0200

    chore: Add tests for booking cancelations

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,14 +1,16 @@
 const BookingPoC = artifacts.require('BookingPoC');
 
 module.exports = function (deployer, network, accounts) {
-  console.log('Network:', network);
-
-  const lifTokenAddress = (network == 'mainnent')
-    ? '0xeb9951021698b42e4399f9cbb6267aa35f82d59d'
-    : '0x5FDFBa355A30FB00ee12965cf3a1c24CA8DF77FB';
-  console.log('Accounts:', accounts);
-  deployer.deploy(BookingPoC, accounts[0], lifTokenAddress, 4)
-  .then(function (bookingContract) {
-    console.log('BookingPoC address:', bookingContract.address);
-  });
+  if (network != 'development') {
+    console.log('Network:', network);
+    const bookingEnds = parseInt(new Date(2018,7,6).getTime() / 1000)
+    const lifTokenAddress = (network == 'mainnent')
+      ? '0xeb9951021698b42e4399f9cbb6267aa35f82d59d'
+      : '0x5FDFBa355A30FB00ee12965cf3a1c24CA8DF77FB';
+    console.log('Accounts:', accounts);
+    deployer.deploy(BookingPoC, accounts[0], lifTokenAddress, 4, bookingEnds)
+    .then(function (bookingContract) {
+      console.log('BookingPoC address:', bookingContract.address);
+    });
+  }
 };
