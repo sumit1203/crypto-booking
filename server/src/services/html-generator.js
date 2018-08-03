e81ad2a8b63a6253057e89c1f76e1c2a79b744fe
Juan Ignacio Gipponi <beogip@gmail.com>
2018-08-03 08:53:23 -0300

    chore(html-generator): remove console.log

--- server/src/services/html-generator.js
+++ server/src/services/html-generator.js
@@ -34,7 +34,6 @@ td.full-width-mobile { width: auto !important; }
 };
 
 const instructionsBody = (paymentType, txs, bookingHash, bookingIndex) => {
-  console.log(paymentType)
   const instructionsHtml = `
 <!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title></title><!--[if !mso]><!-- --><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css">#outlook a { padding:0; }
           .ReadMsgBody { width:100%; }
