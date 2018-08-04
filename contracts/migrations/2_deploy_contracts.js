475e2e28dbc1de950c7e72c5ba009f81f5599e19
AugustoL <augusto.lemble@gmail.com>
2018-08-04 23:41:58 +0300

    chore: Remove unused LifToken and add LifTokenTest

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,16 +1,30 @@
 const BookingPoC = artifacts.require('BookingPoC');
+const LifTokenTest = artifacts.require('LifTokenTest');
 
 module.exports = function (deployer, network, accounts) {
-  if (network != 'development') {
-    console.log('Network:', network);
-    const bookingEnds = parseInt(new Date(2018,7,6).getTime() / 1000)
-    const lifTokenAddress = (network == 'mainnent')
-      ? '0xeb9951021698b42e4399f9cbb6267aa35f82d59d'
-      : '0x5FDFBa355A30FB00ee12965cf3a1c24CA8DF77FB';
-    console.log('Accounts:', accounts);
+  console.log('Migration on network:', network);
+  const bookingEnds = parseInt(new Date(2018,7,6).getTime() / 1000);
+
+  function deployBooking(lifTokenAddress){
     deployer.deploy(BookingPoC, accounts[0], lifTokenAddress, 4, bookingEnds)
     .then(function (bookingContract) {
       console.log('BookingPoC address:', bookingContract.address);
+      bookingContract.addRooms('pure-cozy', 5);
+      bookingContract.addRooms('white-brown-confort', 5);
     });
   }
+
+  if (network != 'development') {
+    deployBooking((network == 'mainnent')
+      ? '0xeb9951021698b42e4399f9cbb6267aa35f82d59d'
+      : '0x5FDFBa355A30FB00ee12965cf3a1c24CA8DF77FB');
+  } else {
+    deployer.deploy(LifTokenTest).then(function (lifToken) {
+      console.log('LifTokenTest address:', lifToken.address);
+      for (var i = 0; i < 10; i++)
+        lifToken.faucetLif({from: accounts[i]});
+      deployBooking(lifToken.address);
+    });
+  }
+
 };
