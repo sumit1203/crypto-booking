e66660769d2e2f713bd12fc68d25704a03365f52
skkr <nico.sikker@gmail.com>
2018-08-05 10:59:09 -0300

    Redesigned the hero and next section

--- app/src/components/HeroSection/index.jsx
+++ app/src/components/HeroSection/index.jsx
@@ -5,25 +5,34 @@ import LogoEB from '../../img/content/logo-ethberlin.png'
 export default class HeroSection extends React.Component {
   render () {
     return (
-      <article>
-        <div className="section-wrapper py-5 bg--gradient">
-          <div className="container">
-            <div className="index-banner__inner text-center">
-              <div className="row justify-content-md-center">
-                <h3 className="col-md-10 mb-1 text-white lead">
+      <article className="bg--gradient" style={{position: 'relative'}}>
+        <div style={{
+          opacity: .6,
+          position: 'absolute',
+          top: '0', left: '0', bottom: '0', right: '0',
+          backgroundImage: 'url(https://windingtree.com/assets/img/banners/lif-token-bg.svg)',
+          backgroundPosition: 'right center, left center',
+          backgroundRepeat: 'no-repeat, no-repeat',
+          backgroundSize: 'contain, auto',
+        }}></div>
+        <div className="section-wrapper py-md-3 py-lg-5" style={{position: 'relative', zIndex: 10}}>
+          <div className="container py-0 py-md-1 py-lg-2">
+            <div className="row justify-content-center text-center">
+              <div className="col-sm-11 col-lg-10">
+                <h3 className="mb-1 text-white lead">
                   ETH Berlin accomodation service
                 </h3>
-                <p className="col-md-10 mb-2 text-white h1">
+                <p className="mb-2 text-white h1">
                   Book a room and save <span className="font--alt">20%</span> with Winding Tree
                 </p>
                 <a href="#pick-room" className="btn btn-primary btn-lg">
-                  <span className="h5">Book with discount</span>
+                  <span>Book with discount</span>
                 </a>
               </div>
             </div>
           </div>
         </div>
-        <a className="bg-white block-shadow d-block" style={{padding: '16px 0 14px 0'}} href="https://ethberlin.com"
+        <a className="bg-white block-shadow d-block" style={{padding: '21px 0 18px 0', position: 'relative'}} href="https://ethberlin.com"
            title="Visit the oficial website.">
           <div className="container">
             <div className="row">
