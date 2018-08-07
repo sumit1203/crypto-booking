7b98877b09a7b2a70db8947e33dd3e53a7850197
beogip <beogip@gmail.com>
2018-08-06 22:47:15 -0300

    feat(MyBookingSection): add error alert

--- app/src/components/MyBookingSection/index.jsx
+++ app/src/components/MyBookingSection/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import $ from 'jquery'
 import EmailSentModal from './EmailSentModal'
 import DeleteInstructionsModal from './DeleteInstructionsModal'
@@ -50,16 +50,17 @@ export default class MyBookingSection extends React.Component {
       const {bookingHash, bookingIndex} = this.state
       const data = {bookingHash, bookingIndex}
       // TODO check this when server can handle this request
-      const response = await (await fetch(SIGNER_API + '/booking', {
+      const response = await (await fetch(SIGNER_API + '/api/booking', {
         method: 'DELETE',
         body: JSON.stringify(data),
         headers: {
           'Content-Type': 'application/json'
         }
       })).json()
-      if (response.status > 400) {
-        console.error(response.code)
-        alert(response.long || response.short)
+      if (response.status >= 400) {
+        console.error(response)
+        this.setState({errorMessage: response.long, loading: false}, this.showErrorAlert)
+        return
       }
       this.setState({cancelTx: {
         to: response.tx.to,
@@ -73,48 +74,63 @@ export default class MyBookingSection extends React.Component {
 
   }
 
+  showErrorAlert = () => {
+    $('.alert').addClass('show')
+    setTimeout(() => {
+      $('.alert').removeClass('show')
+    }, 3000)
+  }
+
   render () {
-    const {cancelTx} = this.state
+    const {cancelTx, errorMessage} = this.state
     return (
-      <article className="py-3 py-md-5 border-bottom" id="my-booking">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-8 text-center">
-              <h2 className="mb-1"> My Booking </h2>
-              <p className="mb-2"> Please enter the data below. </p>
-              <form onSubmit={this.onSubmit}>
-                <div className="form-group text-left">
-                  <label htmlFor="userBookingHash"> <b>Booking hash</b> </label>
-                  <input className="form-control form-control-lg mb-2"
-                         id="userBookingHash"
-                         placeholder="Booking hash"
-                         autoComplete="off"
-                         onChange={this.onHashChange}
-                         type="text"
-                         required/>
-                </div>
-                <div className="form-group text-left">
-                  <label htmlFor="userBookingIndex"> <b>Booking index</b> </label>
-                  <input className="form-control form-control-lg mb-2"
-                         id="userBookingIndex"
-                         placeholder="Booking Index"
-                         autoComplete="off"
-                         onChange={this.onIndexChange}
-                         type="text"
-                         required/>
-                </div>
-                <div className="form-group">
-                  <button className="btn btn-primary btn-lg" type="submit"> Retrieve booking data</button>
-                  <br/>
-                  <button className="btn btn-link btn-sm text-danger"style={{marginTop: '.5em'}} type="button" onClick={this.onCancel}> Cancel my Booking</button>
-                </div>
-              </form>
+      <Fragment>
+        <div className="alert fade fixed-top alert-danger text-center" role="alert">
+          <span>{errorMessage}</span>
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <article className="py-3 py-md-5 border-bottom" id="my-booking">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-md-8 text-center">
+                <h2 className="mb-1"> My Booking </h2>
+                <p className="mb-2"> Please enter the data below. </p>
+                <form onSubmit={this.onSubmit}>
+                  <div className="form-group text-left">
+                    <label htmlFor="userBookingHash"> <b>Booking hash</b> </label>
+                    <input className="form-control form-control-lg mb-2"
+                           id="userBookingHash"
+                           placeholder="Booking hash"
+                           autoComplete="off"
+                           onChange={this.onHashChange}
+                           type="text"
+                           required/>
+                  </div>
+                  <div className="form-group text-left">
+                    <label htmlFor="userBookingIndex"> <b>Booking index</b> </label>
+                    <input className="form-control form-control-lg mb-2"
+                           id="userBookingIndex"
+                           placeholder="Booking Index"
+                           autoComplete="off"
+                           onChange={this.onIndexChange}
+                           type="text"
+                           required/>
+                  </div>
+                  <div className="form-group">
+                    <button className="btn btn-primary btn-lg" type="submit"> Retrieve booking data</button>
+                    <br/>
+                    <button className="btn btn-link btn-sm text-danger"style={{marginTop: '.5em'}} type="button" onClick={this.onCancel}> Cancel my Booking</button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-        <EmailSentModal/>
-        {cancelTx && <DeleteInstructionsModal {...cancelTx}/>}
-      </article>
+          <EmailSentModal/>
+          {cancelTx && <DeleteInstructionsModal {...cancelTx}/>}
+        </article>
+      </Fragment>
     )
   }
 }
