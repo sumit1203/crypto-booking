395a29d7a9828f1b81164d2805734a9a0984587b
AugustoL <augusto.lemble@gmail.com>
2018-08-07 17:31:04 +0300

    fix: Change phone verification in server

--- server/src/models/Booking.js
+++ server/src/models/Booking.js
@@ -20,7 +20,7 @@ function _isBirthDate (birthDate) {
 }
 
 function _isPhone (phone) {
-  const re = /^\+\d{13}$/;
+  const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
   return re.test(String(phone));
 }
 
