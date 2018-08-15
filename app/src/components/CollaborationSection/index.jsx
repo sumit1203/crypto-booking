c11d0f8fe374ec45c54cd823f2c63a7b8a6ef2bc
skkr <nico.sikker@gmail.com>
2018-08-15 20:35:03 -0300

    Removed inline style from collaboration block

--- app/src/components/CollaborationSection/index.jsx
+++ app/src/components/CollaborationSection/index.jsx
@@ -6,10 +6,7 @@ export default class CollaborationSection extends React.Component {
   render() {
 
     return (
-      <article className="section-wrapper py-5" style={{
-        boxShadow: '0 5px 30px rgba(0,0,0,.05) inset',
-        //padding: '150px 0',
-      }}>
+      <article className="section-wrapper py-5">
           <div className="container py-lg-1">
               <div className="row justify-content-center">
                 <div className="col-md-10 col-lg-9 text-center">
@@ -17,7 +14,7 @@ export default class CollaborationSection extends React.Component {
                     Winding Tree provides ETH Berlin accomodation services together with our
                     partner Botique Hotel i31.
                   </p>
-                  <p className="h4"style={{fontWeight: 'normal'}}>
+                  <p className="h4 lead">
                     We offer a <b>20% discount</b> to hackers and engineers
                     who pay with <AnchorLink href="#paying-with-lif">Lif token</AnchorLink>.
                   </p>
