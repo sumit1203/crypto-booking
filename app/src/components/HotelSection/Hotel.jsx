1d32163824ace6f6ff14e28a1de180dccc78f953
skkr <nico.sikker@gmail.com>
2018-08-21 11:31:44 -0300

    Adjustments to map design. Fixed zomm control position.

--- app/src/components/HotelSection/Hotel.jsx
+++ app/src/components/HotelSection/Hotel.jsx
@@ -14,21 +14,22 @@ export default class Hotel extends React.Component {
         <div className="container">
 
           <div className="row flex-column flex-md-row align-items-center">
-            <div className="col-sm-12 col-md-5 col-lg-8 mb-1 mb-md-0">
+
+            <div className="col-sm-12 col-md-5 col-lg-7 mb-1 mb-md-0">
               <h2 className="mb-1 text-white hotel-name"> {name} </h2>
               <MarkdownRenderer markdown={description} className="text-white h4 lead mb-1 hotel-info"/>
               <p className="text-white">
                 <b>For more info please check </b>
-                <a className="ml-sm-1 ml-md-0 ml-lg-1 btn btn-sm btn-secondary" href={url} target="_blank" rel="noopener noreferrer">&nbsp;{url}</a>
+                <a className="ml-sm-1 ml-md-0 ml-lg-1 btn btn-sm btn-secondary"
+                  href={url} target="_blank" rel="noopener noreferrer">&nbsp;{url}
+                </a>
               </p>
           </div>
-          <div className="col-sm-12 col-md-7 col-lg-4 text-center">
-            <div className="mb-2 mt-2 mt-md-1 mb-md-1 map-container">
+
+          <div className="col-sm-12 col-md-7 col-lg-5 text-center">
+            <div className="mb-2 mt-2 mt-md-1 mb-md-1 map-container float-none  float-md-right">
               <HotelMap hotelLocation={hotelLocation} hotelName={name}/>
             </div>
-            {/* <AnchorLink href="#pick-room" className="btn btn-outline-light">
-              Pick a room
-            </AnchorLink> */}
           </div>
         </div>
 
