57c23b9214ebd056189c170679264aba55c3eae7
Pablo Alice <pabloalice95@gmail.com>
2018-08-23 18:10:45 -0300

    update web3 provider for prod

--- app/src/config.js
+++ app/src/config.js
@@ -1,6 +1,6 @@
 const configProd = {
   HOTEL_URL: 'https://demo-api.windingtree.com/hotels/0x4Ee2BC4804D1A75AEBa5C76D25cc2c036B136140',
-  WEB3_PROVIDER: 'https://ropsten.infura.io/CusDXRIFRTuTeUQlhKjc',
+  WEB3_PROVIDER: 'https://mainnet.infura.io/v3/801e69a40e3e49b786cd42e8fb16afa2',
   BOOKING_POC_ADDRESS: '0xA83f78A5b3490b9D6A45B6ada3fF31FAf752566D',
   SIGNER_API: 'https://crypto-booking-server.windingtree.com',
   CAPTCHA_SITE_KEY: '6LfKmmcUAAAAADUI1_CpxzyQ1JHz_bYiQ6Tw3vPF',
