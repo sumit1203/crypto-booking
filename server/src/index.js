09063397a6786e91ca31c6c3ecd72a08b0c4a206
beogip <beogip@gmail.com>
2018-08-29 14:59:26 -0300

    refactor(env): move .env to the root folder

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,13 @@ const path = require('path');
 require('dotenv').config({
   path: path.resolve(
     process.cwd(),
+    '../',
     '.env',
   ),
 });
 
 const { startServer } = require('./app');
 const { SERVER_PORT } = require('./config');
-
 const server = startServer(SERVER_PORT);
 
 module.exports = {
