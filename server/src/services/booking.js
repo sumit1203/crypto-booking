fc7304e34fcef0f6ab5f29736424f5bbd1aa0d78
Luis Dueñas <luis.alonso.duenas.m@gmail.com>
2018-08-30 12:24:04 -0700

    fix typo

--- server/src/services/booking.js
+++ server/src/services/booking.js
@@ -50,7 +50,7 @@ function _prepareForExport (bookingModel, privateKey) {
   booking.personalInfo = bookingModel.decryptPersonalInfo(privateKey);
   booking.fromDate = bookingModel.getFromDate();
   booking.toDate = bookingModel.getToDate();
-  booking.remaindingMinues = bookingModel.getRemaindingMinutes();
+  booking.remaindingMinutes = bookingModel.getRemaindingMinutes();
   return booking;
 }
 
