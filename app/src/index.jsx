a96948a58e095f13570466d9cf14527d55ba8675
Pablo Alice <pabloalice95@gmail.com>
2018-09-06 21:26:13 -0300

    update wt-ui dependency

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes';
-import 'windingtree-ui/src/css/app.scss';
+import '@windingtree/wt-ui/dist/styles.css';
 import '../src/css/app.scss';
 
 ReactDOM.render(
