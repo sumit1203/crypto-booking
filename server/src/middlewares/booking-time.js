75e994c97d19b09cdefe6ec5cb414cbcdb04f80e
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-09-10 13:07:30 +0300

    feat(Booking): Add functions to enable and disble booking period checks

--- server/src/middlewares/booking-time.js
+++ server/src/middlewares/booking-time.js
@@ -1,14 +1,26 @@
 const { handleApplicationError } = require('../errors');
 const { BOOKING_PERIOD_START, BOOKING_PERIOD_END } = require('../config');
 
+let isBookingPeriodOff = false;
+
+const turnOffBookingPeriod = () => {
+  isBookingPeriodOff = true;
+};
+
+const turnOnBookingPeriod = () => {
+  isBookingPeriodOff = false;
+};
+
 const isBookingPeriod = function (req, res, next) {
-  const today = Date.now();
+  if (isBookingPeriodOff) return next();
+
+  const today = new Date(Date.now());
   if (today.getTime() < BOOKING_PERIOD_START.getTime()) {
     return next(handleApplicationError('BookingPeriodStart'));
   }
 
   if (BOOKING_PERIOD_END.getTime() < today.getTime()) {
-    return next('BookingPeriodEnd');
+    return next(handleApplicationError('BookingPeriodEnd'));
   }
 
   next();
@@ -16,4 +28,6 @@ const isBookingPeriod = function (req, res, next) {
 
 module.exports = {
   isBookingPeriod,
+  turnOffBookingPeriod,
+  turnOnBookingPeriod,
 };
