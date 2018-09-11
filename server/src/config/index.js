d6a18cd862329f63256d48d9c82ba167b0544c71
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-09-11 13:51:25 +0300

    chore(logger): Add winston as logger

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,5 +1,19 @@
+const winston = require('winston');
+
 if (!process.env.NODE_ENV) {
   throw new Error('Must set NODE_ENV');
 }
 
-module.exports = require(`./${process.env.NODE_ENV}`);
+module.exports = Object.assign(
+  {
+    logger: winston.createLogger({
+      level: 'info',
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.simple(),
+        }),
+      ],
+    }),
+  },
+  require(`./${process.env.NODE_ENV}`)
+);
