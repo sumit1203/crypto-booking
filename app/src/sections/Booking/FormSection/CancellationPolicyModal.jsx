ed39531d6b9c210459fe83d74dd492e69ad3f33c
Pablo Alice <pabloalice95@gmail.com>
2018-09-12 13:20:47 -0300

    fix(app): update cancellation policy modal

--- app/src/sections/Booking/FormSection/CancellationPolicyModal.jsx
+++ app/src/sections/Booking/FormSection/CancellationPolicyModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const CancellationPolicyModal = () => (
   <div className="modal" id="modalCancellationPolicy" tabIndex="-2" role="dialog">
@@ -6,50 +6,41 @@ const CancellationPolicyModal = () => (
       <div className="modal-content">
         <div className="modal-header">
           <h5 className="modal-title">
-Hotel cancellation & refund policy
+            Hotel cancellation & refund policy
           </h5>
           <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-            <i className="mdi mdi-close" />
+            <i className="mdi mdi-close"/>
           </button>
         </div>
 
         <div className="modal-body">
           <p className="mb-1">
-Please note the cancellation policy before committing to a reservation.
+            Please note the cancellation policy before committing to a reservation.
           </p>
 
           <ul>
-            <li>
-              <b>
-Booking canceled a week before:
-              </b>
-              {' '}
-50% refund.
+            <li className="mb-1">
+              Any cancellation more than a 7 days prior to checkin date will be refunded
+              by 50% of the price of booking.
             </li>
-            <li>
-              <b>
-Booking canceled a day before:
-              </b>
-              {' '}
-25% refund.
+            <li className="mb-1">
+              Any cancellation between a 7 and 1 day before checking date will be refunded
+              by 25% of the price of booking.
             </li>
-            <li>
-              <b>
-Booking canceled in the last 24 hours:
-              </b>
-              {' '}
-10% refund.
+            <li className="mb-1">
+              Any cancellation less than 1 day before checking will be refunded
+              by 10% of the price of booking. Cancellation past the booking time is not possible.
             </li>
           </ul>
         </div>
         <div className="modal-footer">
           <button type="button" className="btn btn-primary" data-dismiss="modal">
-Accept
+            Accept
           </button>
         </div>
       </div>
     </div>
   </div>
-);
+)
 
-export default CancellationPolicyModal;
+export default CancellationPolicyModal
